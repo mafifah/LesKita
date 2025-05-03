f7ce03626b8fa6779f41a1440a174424dc1aa1cf
M Afifah <mafifah.usr@gmail.com>
2025-05-04 00:20:55 +0700

    Styling Beranda (Belum selesai)

---
+++ LesKita/UI Model/uimT0Guru.cs
@@ -0,0 +1,10 @@
+﻿using LesKita.Model;
+
+namespace LesKita;
+
+public class uimT0Guru : T0Guru
+{
+    public int Jarak { get; set; } = 0;
+    public int JumlahKlien { get; set; } = 0;
+    public string? UrlFoto { get; set; } = string.Empty;
+}
