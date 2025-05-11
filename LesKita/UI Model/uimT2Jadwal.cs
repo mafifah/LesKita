d34d33ceb30005b7541ef8909576d8e29b524fb3
M Afifah <mafifah.usr@gmail.com>
2025-05-11 20:38:03 +0700

    Atur layout RcpJadwal

---
+++ LesKita/UI Model/uimT2Jadwal.cs
@@ -0,0 +1,17 @@
+﻿namespace LesKita;
+
+public class uimT2Jadwal : T2Jadwal
+{
+    public string? Siswa_Nama { get; set; }
+    public DateTimeOffset? Siswa_TanggalLahir { get; set; }
+    public DateTimeOffset? Siswa_Usia { get; set; }
+    public string? Siswa_Alamat { get; set; }
+    public string? Siswa_Email { get; set; }
+
+    public string? Mentor_Nama { get; set; }
+    public DateTimeOffset? Mentor_TanggalLahir { get; set; }
+    public DateTimeOffset? Mentor_Usia { get; set; }
+    public string? Mentor_Alamat { get; set; }
+    public string? Mentor_Email { get; set; }
+    public string WarnaKode { get; set; }
+}
